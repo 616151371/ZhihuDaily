c643e045b8a2cfdf103fe67509d2f79c0fa5b203
lihaijun <179816609@qq.com>
2020-12-08 11:31:34 +0800

    列表功能完善，todo 点击事件

--- zhihu_daily/lib/pages/home_page.dart
+++ zhihu_daily/lib/pages/home_page.dart
@@ -5,6 +5,9 @@ import 'dart:convert';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:zhihu_daily/utils/dace_util.dart';
 import 'package:zhihu_daily/views/zhi_hu_swiper_pagination.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:toast/toast.dart';
+import 'package:date_format/date_format.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -16,14 +19,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static const int DATE_TYPE = 1901;
   List<ZhihuNewsTopStory> _headerList = new List<ZhihuNewsTopStory>();
   List<ZhihuNewsStory> _newsList = new List<ZhihuNewsStory>();
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: true);
   DateTime today;
+  DateTime beforeDay;
 
   @override
   void initState() {
     today = getNow();
-    queryData();
     super.initState();
   }
 
@@ -51,20 +57,33 @@ class _HomePageState extends State<HomePage> {
                 icon: CircleAvatar(
                     backgroundImage: AssetImage("images/account_avatar.png")),
                 onPressed: () {
-                  print(">>>>>> avatar pressed");
                 })
           ],
         ),
-        body: ListView.builder(
-          itemCount: 50,
-          shrinkWrap: true,
-          itemBuilder: (context, index) {
-            if (index == 0) {
-              return _getHeader();
-            }
-            return _getItem(index);
-          },
-        ));
+        body: SmartRefresher(
+            onRefresh: _onRefresh,
+            onLoading: _onLoading,
+            enablePullUp: true,
+            enablePullDown: true,
+            controller: _refreshController,
+            child: ListView.builder(
+              itemCount: _newsList.length + 1,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return _getHeader();
+                }
+                return _getItem(index - 1);
+              },
+            )));
+  }
+
+  void _onRefresh() {
+    queryData();
+  }
+
+  void _onLoading() {
+    queryBeforeData();
   }
 
   Widget _getHeader() {
@@ -76,6 +95,7 @@ class _HomePageState extends State<HomePage> {
         width: size.width,
         height: size.width,
         child: Swiper(
+            autoplay: true,
             itemCount: _headerList.length,
             itemHeight: MediaQuery.of(context).size.width,
             pagination: ZhiHuSwiperPagination(),
@@ -118,20 +138,35 @@ class _HomePageState extends State<HomePage> {
                     padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
                     child: Text(story.title,
                         maxLines: null,
-                        style: TextStyle(fontWeight:FontWeight.w400,fontSize: 25, color: Colors.white))),
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 25,
+                            color: Colors.white))),
                 Padding(
                     padding: EdgeInsets.fromLTRB(20, 0, 20, 30),
-                    child:
-                    Text(story.hint, style: TextStyle(color: Colors.white60)))
+                    child: Text(story.hint,
+                        style: TextStyle(color: Colors.white60)))
               ])
         ]);
   }
 
   Widget _getItem(int index) {
-    if (index - 1 > _newsList.length - 1) {
+    if (index > _newsList.length - 1) {
       return null;
     }
-    ZhihuNewsStory story = _newsList[index - 1];
+    ZhihuNewsStory story = _newsList[index];
+    if (story.type == DATE_TYPE) {
+      return Container(
+          margin: EdgeInsets.fromLTRB(10, 10, 0, 10),
+          child: Row(children: [
+            Text(
+              story.title,
+              style: TextStyle(color: Colors.grey),
+            ),
+            Expanded(
+                child: Divider(indent: 10, height: 2, color: Colors.grey[500]))
+          ]));
+    }
     Size size = MediaQuery.of(context).size;
     return Container(
       width: size.width,
@@ -172,12 +207,55 @@ class _HomePageState extends State<HomePage> {
       if (response.statusCode == 200) {
         ZhihuNewsEntity newsEntity =
             new ZhihuNewsEntity().fromJson(json.decode(response.toString()));
-        _newsList.addAll(newsEntity.stories);
-        _headerList.addAll(newsEntity.topStories);
-        setState(() {});
+        setState(() {
+          beforeDay = today;
+          _newsList.clear();
+          _headerList.clear();
+          _newsList.addAll(newsEntity.stories);
+          _headerList.addAll(newsEntity.topStories);
+        });
+      } else {
+        Toast.show(response.statusMessage, context);
+      }
+      _refreshController.refreshCompleted();
+    } catch (e) {
+      print(e);
+      Toast.show("网络错误", context);
+      _refreshController.refreshFailed();
+    }
+  }
+
+  void queryBeforeData() async {
+    try {
+      var url = "http://news-at.zhihu.com/api/4/news/before/" +
+          formatDate(beforeDay, [yyyy, mm, dd]);
+      Response response = await Dio().get(url);
+      if (response.statusCode == 200) {
+        ZhihuNewsEntity newsEntity =
+            new ZhihuNewsEntity().fromJson(json.decode(response.toString()));
+        if (newsEntity == null ||
+            newsEntity.stories == null ||
+            newsEntity.stories.length == 0) {
+          _refreshController.loadNoData();
+          return;
+        }
+        setState(() {
+          beforeDay = beforeDay.subtract(Duration(days: 1));
+          ZhihuNewsStory dateTypeStory = ZhihuNewsStory();
+          dateTypeStory.type = DATE_TYPE;
+          dateTypeStory.title = formatDate(beforeDay, [mm, ' 月 ', d, ' 日']);
+          _newsList.add(dateTypeStory);
+          _newsList.addAll(newsEntity.stories);
+        });
+        _refreshController.loadComplete();
+      } else {
+        Toast.show(response.statusMessage, context);
+        _refreshController.loadFailed();
       }
     } catch (e) {
       print(e);
+      Toast.show("网络错误", context);
+      _refreshController.loadFailed();
     }
   }
 }
